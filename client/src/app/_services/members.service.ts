e078e42acb80701cef3c6e2266913296a5f1bf99
IGE-XAO-BU\gmarkov <gmarkov@ige-xao.com>
2022-06-14 12:02:24 +0300

    section 9 part 107, css and angular

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/Member'
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  httpOptions = {
+    headers : new HttpHeaders({
+      Authorization : 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
+    }),
+  }
+
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  GetMembers()
+  {
+    
+    
+    return this.http.get<Member[]>(this.baseUrl + 'users', this.httpOptions);
+  }
+  GetMember(username : string)
+  {
+    return this.http.get<Member>(this.baseUrl + "users/" + username + this.httpOptions);
+  }
+}
