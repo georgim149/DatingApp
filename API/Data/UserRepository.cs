ab291bd5f26ad841f052b77b79f4f0c9795fd2b8
IGE-XAO-BU\gmarkov <gmarkov@ige-xao.com>
2022-06-23 10:27:21 +0300

    APIpaging

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -29,11 +30,13 @@ namespace API.Data
             .SingleOrDefaultAsync();
         }
 
-        public async Task<IEnumerable<MemberDTO>> GetMembersAsync()
+        public async Task<PagedList<MemberDTO>> GetMembersAsync(UserParams userParams)
         {
-            return await context.Users
+            var result = context.Users
             .ProjectTo<MemberDTO>(mapper.ConfigurationProvider)
-            .ToListAsync();
+            .AsNoTracking();
+            return await PagedList<MemberDTO>.CreateAsync(result, userParams.PageNumber, userParams.PageSize);
+            
         }
 
         public async Task<AppUser> GetUserByIDAsync(int id)
